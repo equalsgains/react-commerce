41b47f38b26f58c788b9ccc5d12332f54cf1d2c5
equalsgains <equalsgains@gmail.com>
2020-09-24 23:43:39 -0600

    started on context API

---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+export const initialState = {
+    basket: [],
+    
+}
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'ADD_TO_BASKET':
+            // LOGIG FOR ADDING ITEM TO BASKET
+            break;
+        case 'REMOVE_FROM_BASKET':
+            break;
+    }
+}
